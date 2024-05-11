9e209123f8d97ae3ae62a695441e67ebca8c244d
Nethrani Rajapaksha <159509177+NethraniRajapaksha@users.noreply.github.com>
2024-05-12 04:06:30 +0530

    Add Note Screen Add Database

--- app/src/main/java/com/example/labexam4/Home.kt
+++ app/src/main/java/com/example/labexam4/Home.kt
@@ -3,26 +3,30 @@ package com.example.labexam4
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.labexam4.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+class Home : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.add.setOnClickListener{
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent) // This line starts the AddNoteActivity
+        }
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val login = findViewById<Button>(R.id.GO)
-        login.setOnClickListener {
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
         }
     }
 }
