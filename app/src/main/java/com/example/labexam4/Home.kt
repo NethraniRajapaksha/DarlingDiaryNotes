a8a1d10fdba2916ecd5b6516fdc50ec8d93efdb8
Nethrani Rajapaksha <159509177+NethraniRajapaksha@users.noreply.github.com>
2024-05-12 15:42:01 +0530

    Display Details

--- app/src/main/java/com/example/labexam4/Home.kt
+++ app/src/main/java/com/example/labexam4/Home.kt
@@ -8,14 +8,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.labexam4.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var db : DarlingDiaryDataBase
+
+    private lateinit var notesAdapter: NotesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = DarlingDiaryDataBase(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(),this)
+
+        binding.noteRecycerView.layoutManager = LinearLayoutManager(this)
+        binding.noteRecycerView.adapter = notesAdapter
 
         binding.add.setOnClickListener{
             val intent = Intent(this, AddNoteActivity::class.java)
@@ -28,5 +38,10 @@ class Home : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
